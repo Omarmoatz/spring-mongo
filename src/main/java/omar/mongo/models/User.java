bcbe6d7a4f794fd794bc64ea7f7195bdfa78121d
Omarmoatz <omar.raouf.002@gmail.com>
2025-09-25 16:47:12 +0300

    add user model

---
+++ src/main/java/omar/mongo/models/User.java
@@ -0,0 +1,24 @@
+package omar.mongo.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+    
+    @Id
+    public String id;
+    public String username;
+    public String email;
+    public String password;
+}
